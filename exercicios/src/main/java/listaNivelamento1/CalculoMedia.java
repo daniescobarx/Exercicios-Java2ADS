a3fcb888fa11df32ca7c92789d3308c0008de2e3
daniescobarx <seu-email@exemplo.com>
2024-02-21 09:14:17 -0300

    add ex

---
+++ exercicios/src/main/java/listaNivelamento1/CalculoMedia.java
@@ -0,0 +1,11 @@
+package school.sptech;
+
+public class CalculoMedia {
+    public static void main(String[] args) {
+        String nome = "Damiela";
+        Double nota1 = 8.0;
+        Double nota2 = 9.0;
+        Double media = (nota1+nota2)/2;
+        System.out.println("Olá "+ nome+" Sua média foi de "+media);
+    }
+}
