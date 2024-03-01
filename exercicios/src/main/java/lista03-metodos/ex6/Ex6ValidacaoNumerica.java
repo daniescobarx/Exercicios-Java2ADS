c3e890e806bb76b1f665b43c465dc5624197a79a
daniescobarx <daniela.mollo@sptech.school>
2024-03-01 11:49:25 -0300

    add ex

---
+++ exercicios/src/main/java/lista03-metodos/ex6/Ex6ValidacaoNumerica.java
@@ -0,0 +1,25 @@
+package school.sptech.ex6;
+
+import java.util.Scanner;
+
+public class Ex6ValidacaoNumerica {
+
+  public static void main(String[] args) {
+    Ex6ValidacaoNumericaMetodos metodos = new Ex6ValidacaoNumericaMetodos();
+    Scanner scanner = new Scanner(System.in);
+
+    while (true) {
+      System.out.println("Digite um número:");
+      Integer numero = scanner.nextInt();
+
+      if (numero < 0) {
+        System.out.println("Fim da execução");
+        break;
+      }
+
+      metodos.verificarPrimo(numero);
+
+    }
+  }
+  }
+
---
+++ exercicios/src/main/java/lista03-metodos/ex6/Ex6ValidacaoNumericaMetodos.java
@@ -0,0 +1,19 @@
+package school.sptech.ex6;
+public class Ex6ValidacaoNumericaMetodos {
+
+    public void verificarPrimo(Integer numero) {
+        if (numero <= 1) {
+            System.out.println("Não é primo");
+            return;
+        }
+
+        for (int i = 2; i <= Math.sqrt(numero); i++) {
+            if (numero % i == 0) {
+                System.out.println("Não é primo");
+                return;
+            }
+        }
+
+        System.out.println("É primo");
+    }
+}
